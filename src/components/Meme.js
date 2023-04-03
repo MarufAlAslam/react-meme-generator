b04d219e19ee7a77d79742b7dde3a69fec329b7a
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-04-03 14:36:17 +0600

    download option fixed - CORS issue resolved

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -73,6 +73,7 @@ const Meme = () => {
               display: "flex",
               justifyContent: "space-between",
               alignItems: "center",
+              width: "100%",
             }}
           >
             <Button
